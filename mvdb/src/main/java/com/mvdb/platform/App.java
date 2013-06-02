160aababb4a0766ba08f42070b8c2d57c5d4378f
Umesh K <fulcrum@hotmail.com>
2013-06-02 02:24:23 -0700

    Changes to introduce the use of logback for logging.

--- mvdb/src/main/java/com/mvdb/platform/App.java
+++ mvdb/src/main/java/com/mvdb/platform/App.java
@@ -1,13 +1,24 @@
 package com.mvdb.platform;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
+
 /**
- * Hello world!
- *
+ * 
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	private static Logger logger = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		logger.error("error");
+		logger.warn("warning");
+		logger.info("info");
+	    // print internal state
+	    LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+	    StatusPrinter.print(lc);
+	}
 }
