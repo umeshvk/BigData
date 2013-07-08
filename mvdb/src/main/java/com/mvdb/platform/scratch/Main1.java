2dcffdbf7c26e7b9da70142b034a61c48acf5ad4
Umesh K <fulcrum@hotmail.com>
2013-07-07 18:25:29 -0700

    Changes in scratch. Ignore scratch.

---
+++ mvdb/src/main/java/com/mvdb/platform/scratch/Main1.java
@@ -0,0 +1,18 @@
+package com.mvdb.platform.scratch;
+
+public class Main1
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+       System.out.println("com.mvdb.platform.scratch.Main1");
+       for(String str : args) { 
+           System.out.println("Found:" + str); 
+       }
+
+    }
+
+}
