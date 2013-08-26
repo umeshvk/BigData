44fd0ffdd1e71e6502f6a24bd7122cb6500b6cb0
Umesh K <fulcrum@hotmail.com>
2013-08-26 02:26:28 -0700

    Move  Test class to junk

---
+++ junk/src/main/java/com/mvdb/etl/actions/Test.java
@@ -0,0 +1,14 @@
+package com.mvdb.etl.actions;
+
+public class Test
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        ActionUtils.zipFullDirectory("/tmp/testfolder", "/tmp/test.zip");
+    }
+
+}
